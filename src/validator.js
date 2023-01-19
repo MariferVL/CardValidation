aceca569e7cbcecd04b91669f3946c239df934c2
Marifer <mfvl.1186@gmail.com>
2023-01-19 00:04:34 -0300

    TODO stablished

--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,18 @@
+// TODO: Insertar número de tarjeta a validar (texto) en un campo de texto vacío.
+// TODO:  Evitar escribir caracteres  no  numéricos (dígitos 0-9).
+// TODO:  Campo vacío no se valida ==> campo requerido 
+// TODO:  Comunicar a  usuaria si es taerjeta válida o no.
+// TODO:  Ocultar dígitos número de tarjeta con exepción de últimos 4 char.
+// TODO:  metódos de validator (isValid y maskify) deben tener cobertura con pruebas unitarias. ¿???
+// TODO:  proyecto subido a  repo e interfaz "desplegada".
+// TODO:  README que contiene una definición del producto.
+            // cómo pensaste en los usuarios + proceso para definir producto final a nivel de experiencia y de interfaz.
+            // Quiénes son los principales usuarios de producto.
+            // Cuáles son los objetivos de estos usuarios en relación con tu producto.
+            // Cómo crees que el producto que estás creando está resolviendo sus problemas.
+// Requisitos cumplidos ==> posibilidad pedir sesión de Project Feedback con unx coach.
+// TODO:  Mostrar la franquicia de tarjeta
+
 const validator = {
   // ...
 };
