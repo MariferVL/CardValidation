65cdd428f8c20d5aa129b191ecb20f61cfcd2c4c
Marifer <mfvl.1186@gmail.com>
2023-01-19 00:17:47 -0300

    html + css added

--- src/validator.js
+++ src/validator.js
@@ -6,10 +6,10 @@
 // TODO:  metódos de validator (isValid y maskify) deben tener cobertura con pruebas unitarias. ¿???
 // TODO:  proyecto subido a  repo e interfaz "desplegada".
 // TODO:  README que contiene una definición del producto.
-            // cómo pensaste en los usuarios + proceso para definir producto final a nivel de experiencia y de interfaz.
-            // Quiénes son los principales usuarios de producto.
-            // Cuáles son los objetivos de estos usuarios en relación con tu producto.
-            // Cómo crees que el producto que estás creando está resolviendo sus problemas.
+// cómo pensaste en los usuarios + proceso para definir producto final a nivel de experiencia y de interfaz.
+// Quiénes son los principales usuarios de producto.
+// Cuáles son los objetivos de estos usuarios en relación con tu producto.
+// Cómo crees que el producto que estás creando está resolviendo sus problemas.
 // Requisitos cumplidos ==> posibilidad pedir sesión de Project Feedback con unx coach.
 // TODO:  Mostrar la franquicia de tarjeta
 
