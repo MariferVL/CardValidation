823fab56c16df8d8b83182d9d1950a2ff8774ad7
Marifer <mfvl.1186@gmail.com>
2023-01-25 01:57:43 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,57 @@
+import elements from './validator.js';
+
+let result;
+let description;
+
+
+const dangerDiv = document.getElementById("danger");
+const successDiv = document.getElementById("success");
+
+const replacement = document.getElementById("toRemove");
+
+// Create new h3 and p elements
+const tittleBox = document.createElement("h3");
+const msgBox = document.createElement("p");
+
+document.getElementById("validate").addEventListener("click", validate, false);
+
+// Send validation msg to document
+function validate() {
+  //Get card number from user
+  const cardNumber = document.getElementById("cardnumber").value
+  const valid = elements.isValid(cardNumber)
+  const cardBrand = elements.getCardBrand(cardNumber);
+
+
+  if (valid) {
+    //Show success in div#success
+    successDiv.appendChild(tittleBox);
+    successDiv.appendChild(msgBox);
+    document.getElementById("success").style.display = "inline";
+    replacement.replaceWith(successDiv);
+
+    result = "Tarjeta " + cardBrand + "\n" + elements.maskify(cardNumber) + "\n Válida";
+    description = "Operación exitosa. La información bancaria ingresada es correcta.";
+  }
+  else {
+    // Show error message in div#danger
+    dangerDiv.appendChild(tittleBox);
+    dangerDiv.appendChild(msgBox);
+    document.getElementById("danger").style.display = "inline";
+    replacement.replaceWith(dangerDiv);
+
+    result = "Tarjeta " + cardBrand + "\n Inválida.";
+    description = "Debe ingresar nuevamente la información de su tarjeta.";
+  }
+  tittleBox.classList.add("alert-heading");
+
+  // Give h1 and p content
+  const h3Content = document.createTextNode(result);
+  const pContent = document.createTextNode(description);
+
+  tittleBox.appendChild(h3Content);
+  msgBox.appendChild(pContent);
+
+}
+
+
---
+++ validator.js
@@ -0,0 +1,98 @@
+// Test Data: 4137894711755904
+
+let cardDigits;
+
+// Validate card number using Luhn's algorithm
+function isValid(creditCardNumber) {
+  let valid = false;
+
+  // Validate if card number is integer
+  if (Number.isInteger(Number(creditCardNumber))) {
+    // Split input
+    cardDigits = creditCardNumber.split('');
+
+    const reversedCardDigits = cardDigits.reverse();
+
+    // console.log("Paso2 -Numero invertido: ", reversedCardDigits);
+    const stringToNum = reversedCardDigits.map(function (str) {
+      // using map() to convert array of strings to numbers
+      return parseInt(str);
+    });
+
+    // Double value of every 2nd digit
+    for (let index = 1; index < stringToNum.length; index += 2) {
+      const doubleNum = Number(stringToNum[index]) * 2;
+      stringToNum[index] = doubleNum;
+
+      // Check each doubleNum if is greater than 9
+      if (doubleNum > 9) {
+
+        // Add the digits in doubleNum
+        const digitsSum = sumDigits(doubleNum);
+        stringToNum[index] = digitsSum;
+
+
+      }
+    }
+
+    // Calculate the sum using forEach
+    let sumCardDigits = 0;
+    stringToNum.forEach(x => { sumCardDigits += x; });
+
+    // Check if the sum of all digits is 0
+    if (sumCardDigits % 10 === 0) {
+      valid = true;
+    }
+
+  } else {
+    alert("En 'Número de Tarjeta' debes ingresar sólo dígitos.")
+  }
+
+  return valid
+}
+
+// Fuction to sum digits in a number
+function sumDigits(n) {
+  return (--n % 9) + 1;
+}
+
+// Function to get card financial institutuion (Visa, Mastercard or American Express)
+function getCardBrand(creditCardNumber) {
+  const firstNumber = creditCardNumber.charAt(0);
+
+  let brand;
+  switch (firstNumber) {
+  case "3":
+    brand = "American Express";
+    break;
+  case "4":
+    brand = "Visa";
+    break;
+  case "5":
+    brand = "Mastercard";
+    break;
+
+  default:
+    brand = "";
+    break;
+  }
+  return brand;
+}
+
+//replace all digits with # except the last 4.
+function maskify(creditCardNumber) {
+  const listNumbers = creditCardNumber.split("");
+  listNumbers.forEach((element) => { for (let i = listNumbers.indexOf(element); i < listNumbers.length - 4; i++) listNumbers[i] = "#"; });
+
+  const maskedNum = listNumbers.join("");
+
+  //return string
+  return maskedNum;
+}
+
+const components = {
+  isValid, sumDigits, getCardBrand, maskify,
+};
+
+
+export default components;
